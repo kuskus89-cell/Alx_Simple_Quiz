8eebaa7e0ec6ec0ba9d5349d4460c97bc6d25122
kuskus89-cell <sundusnoor89@gmail.com>
2025-12-09 20:39:23 +0000

    Did a quiz

---
+++ quiz.js
@@ -0,0 +1,18 @@
+function checkAnswer () {
+    const correctAnswer="4";
+
+    const userAnswer = document.querySelector('input[name="quiz"]:checked');
+
+    const feedback=document.getElementById('feedback');
+
+    if(!userAnswer){
+        feedback.textContent="Please select an answer before submitting !";
+        return;  
+
+    } if(userAnswer.value === correctAnswer){
+        feedback.textContent="Correct! Well done.";
+    } else{
+        feedback.textContent="That's incorrect. Try again!";
+    }
+}
+document.getElementById('submit-answer') .addEventListener("click", checkAnswer);
