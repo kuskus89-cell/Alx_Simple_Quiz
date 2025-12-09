bf523c16e043f2cf5345c354681ce640d1d801d7
kuskus89-cell <sundusnoor89@gmail.com>
2025-12-09 21:25:57 +0000

    Added a code

--- quiz.js
+++ quiz.js
@@ -1,19 +1,20 @@
 function checkAnswer () {
-    const correctAnswer="4";
+    const correctAnswer = "4";
 
     const userAnswer = document.querySelector('input[name="quiz"]:checked');
 
-    const feedback=document.getElementById('feedback');
+    const feedback = document.getElementById('feedback');
 
     if(!userAnswer){
         feedback.textContent="Please select an answer before submitting!";
         return; 
     }
 
-     if(userAnswer === correctAnswer){
-        feedback.textContent="Correct! Well done.";
+     if(userAnswer.value === correctAnswer){
+        feedback.textContent = "Correct! Well done.";
     } else{
-        feedback.textContent="That's incorrect. Try again!";
+        feedback.textContent = "That's incorrect. Try again!";
     }
 }
-document.getElementById('submit-answer') .addEventListener("click", checkAnswer);
+
+document.getElementById('submit-answer').addEventListener("click", checkAnswer);
