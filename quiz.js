659ef8e8f81f3e0da8d585cc3066f878daf6002b
kuskus89-cell <sundusnoor89@gmail.com>
2025-12-09 21:07:20 +0000

    Added another line of code to fix the error

--- quiz.js
+++ quiz.js
@@ -5,11 +5,16 @@ function checkAnswer () {
 
     const feedback=document.getElementById('feedback');
 
+    const userAnswerValue = userAnswer.Value
+if(userAnswer === correctAnswer){
+    
+}
+
     if(!userAnswer){
         feedback.textContent="Please select an answer before submitting!";
         return;  
 
-    } if("userAnswer.value === correctAnswer"){
+    } if(userAnswer.value === correctAnswer){
         feedback.textContent="Correct! Well done.";
     } else{
         feedback.textContent="That's incorrect. Try again!";
