9da33c617489c9ee7c2bafb5e93d221a93becd90
kuskus89-cell <sundusnoor89@gmail.com>
2025-12-19 21:44:57 +0000

    added a code

--- quiz.js
+++ quiz.js
@@ -10,7 +10,7 @@ function checkAnswer() {
         return; 
     }
 
-     if(userAnswer === correctAnswer){
+     if(userAnswer.value === correctAnswer){
         feedback.textContent = "Correct! Well done.";
     } else{
         feedback.textContent = "That's incorrect. Try again!";
