9e295a18d606ab5ddabcb8aa3f8735e6b895a572
kuskus89-cell <sundusnoor89@gmail.com>
2025-12-19 21:39:10 +0000

    trying

--- quiz.js
+++ quiz.js
@@ -11,14 +11,14 @@ function checkAnswer() {
     }
 
     if(userAnswer === correctAnswer){
-
+    }
      if(userAnswer.value === correctAnswer){
         feedback.textContent = "Correct! Well done.";
     } else{
         feedback.textContent = "That's incorrect. Try again!";
     }
 }
-}
+
 
 
 document.getElementById('submit-answer').addEventListener("click", checkAnswer);
