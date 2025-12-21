1fdeb5dd64a928ae5e18af89f8b7e2474af2f2d5
kuskus89-cell <sundusnoor89@gmail.com>
2025-12-21 17:43:41 +0000

    answer value

--- quiz.js
+++ quiz.js
@@ -1,7 +1,7 @@
 function checkAnswer() {
     const correctAnswer = "4";
 
-    const userAnswer = document.querySelector('input[name="quiz"]:checked');
+    const userAnswer = document.querySelector('input[name="quiz"]:checked').value;
 
     const feedback = document.getElementById('feedback');
 
@@ -10,7 +10,7 @@ function checkAnswer() {
         return; 
     }
 
-     if(userAnswer.value === correctAnswer){
+     if(userAnswer === correctAnswer){
         feedback.textContent = "Correct! Well done.";
     } else{
         feedback.textContent = "That's incorrect. Try again!";
